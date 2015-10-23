ce4ef14f6be319065c8c1fc9e9a7abbf585e685b
Matt Ellen <matt@matthewellen.co.uk>
2015-10-23 20:01:35 +0100

    added imageDataToGreyScale function correct misspelling

--- jsia.js
+++ jsia.js
@@ -1,4 +1,4 @@
-jsai = (function()
+jsia = (function()
 	{
 	    function jsai()
 	    {
@@ -29,6 +29,33 @@ jsai = (function()
 	    jsia.xyToIndex = function(x, y, width)
 	    {
 		return (x + (y * width))*4;
+	    };
+
+	    jsia.imageDataToGreyScale = function(image, useBrightest)
+	    {
+		var grey = new ImageData(image.data, image.width, image.height);
+
+		for(var i = 0; i < grey.data.length; i+=4)
+		{
+		    var r = grey.data[i];
+		    var g = grey.data[i+1];
+		    var b = grey.data[i+2];
+
+		    if(useBrightest)
+		    {
+			var value = Math.max(r, Math.max(g, b));
+		    }
+		    else
+		    {
+			var value = Math.min(r, Math.min(g, b));
+		    }
+
+		    grey.data[i] = value;
+		    grey.data[i+1] = value;
+		    grey.data[i+2] = value;
+		}
+
+		return grey;
 	    };
 
 	    return jsai;
